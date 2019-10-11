10bf8f97952e247f93f2813bc5480a593991f476
Jesper Seipenbusch <jesper.s.js@gmail.com>
2019-10-11 10:20:48 +0200

    Added UI scene

---
+++ Assets/Scripts/Ui_script.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Ui_script : MonoBehaviour
+{
+
+    public bool upgrades;
+    public GameObject mainMenu;
+    public GameObject upgradeMenu;
+
+    void Update()
+    {
+        if(upgrades)
+        {
+            upgradeMenu.SetActive(true);
+            mainMenu.SetActive(false);
+        }
+        else
+        {
+            upgradeMenu.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+    }
+    
+    public void Play()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void GoToUpgrades()
+    {
+        upgrades = !upgrades;
+    }
+
+    public void DoExitGame() 
+    {
+        Application.Quit();
+    }
+}
